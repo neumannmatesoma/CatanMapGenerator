9c419f7d1277d07713539f618d13003f67e79503
Neumann Mate <neumannmatesoma1@gmail.com>
2025-03-02 20:58:40 +0100

    renaming realistic tiles and implementing theme selector

--- website/resources/js/shuffle.js
+++ website/resources/js/shuffle.js
@@ -1,6 +1,6 @@
 function RandomShuffle() {    
     for (let i = 1; i< 20; i++){
-        let rand_pic = Math.floor(getRandomArbitrary(1, 6));
+        let rand_pic = Math.floor(getRandomArbitrary(1, 3));
 
         picChange(`${ore_types[rand_pic]}`, i);
     }
