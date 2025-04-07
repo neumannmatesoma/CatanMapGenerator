629f13058a1e499e464957a7fa90a5f0b7b4777f
Neumann Mate <neumannmatesoma1@gmail.com>
2025-04-07 13:56:40 +0200

    reformatting for better understanding

--- website/resources/js/init.js
+++ website/resources/js/init.js
@@ -64,32 +64,3 @@ function init() {
     // shuffleing the board
     Shuffle();
 }
-
-
-/*
-// variables
-var theme = null; // theme
-var rule1 = null; // 6 & 8 can touch
-var rule2 = null; // 2 & 12 can touch
-var rule3 = null; // same numbers can touch
-var rule4 = null; // same resources can touch
-var rule5 = null; // randomize ports
-var rule6 = null; // atleast one different neighbor
-var pic_path = null; // tile picture path
-var ore_types = ["desert", "brick", "ore", "sheep", "wheat", "wood"] // the types of ores
-
-// div --> map_div_%DIV_NUM%
-// pic --> `url(/resources/img/${THEME}/"${PIC}.png)`
-
-function init() {
-    // initializing theme and options
-    theme = document.getElementById("theme_selector_id").value;
-    
-    // IMPORTANT: if its opened from root folder (CatanMapGenerator)
-    // then "/website/resources/img/${theme}/" is needed
-    pic_path = `/resources/img/${theme}/`;
-
-
-    Shuffle();
-}
-*/
--- website/resources/js/shuffle.js
+++ website/resources/js/shuffle.js
@@ -5,13 +5,13 @@ function Shuffle() {
 
     // allocating resources randomly
     const resources = shuffleArray([...standardResources]);
-    
+
     // allocating numbers
     const numbers = distributeNumbers(resources);
-    
+
     // displaying the board
     DisplayBoard(resources, numbers);
-    
+
 }
 
 // displays the whole board
@@ -19,14 +19,14 @@ function DisplayBoard(resources, numbers) {
     // iterating through all divs
     for (let i = 1; i <= 19; i++) {
         // getting a resource's number
-        const resource = resources[i-1];
+        const resource = resources[i - 1];
 
         // getting a possibility number
-        const number = numbers[i-1];
-        
+        const number = numbers[i - 1];
+
         // changing the picture
         PicChange(resource, i);
-        
+
         if (resource === 'desert') {
             // unloading possibility number if the tile is desert
             UnloadNumberForDesert(i);
@@ -39,7 +39,7 @@ function DisplayBoard(resources, numbers) {
             document.getElementById(`map_div_${i}_dot`).innerHTML = possibility[RollCalculator(number) - 1];
         }
     }
-    
+
     // generating ports
     GenerateBasePorts();
 }
@@ -52,47 +52,47 @@ function distributeNumbers(resources) {
     const shuffledNumbers = shuffleArray([...standardNumbers]);
     // searching for desert's position
     const desertIndex = resources.indexOf('desert');
-    
+
     // the number of the current attempt
     let attempts = 0;
     // the number of the maximum attempts
-    const maxAttempts = 20;
-    
+    const maxAttempts = 100;
+
     while (attempts < maxAttempts) {
         // increase the current attempt counter
         attempts++;
-        
+
         // reseting numbers array on every attempt
         // restarting the distribution of numbers
         numbers.fill(null);
         let success = true;
         let numIndex = 0;
-        
+
         for (let i = 0; i < 19; i++) {
             // skipping desert
             if (i === desertIndex) continue;
-            
+
             // distributing the next number
             const currentNumber = shuffledNumbers[numIndex++];
             numbers[i] = currentNumber;
-            
+
             // validating the number via rules
             if (!validateNumberPlacement(i, numbers)) {
                 success = false;
                 break;
             }
         }
-        
+
         // if everything ok logging success and how much attempts it took
         if (success) {
             console.log(`Számok elosztva ${attempts} próbálkozás után`);
             return numbers;
         }
-        
+
         // if something went wrong the numbers are reshuffled
         shuffleArray(shuffledNumbers);
     }
-    
+
     // fallback to default, random shuffleing if max attempts were exceeded
     console.warn("Nem sikerült tökéletes elosztás, visszaesés alapértelmezettre");
     numbers.fill(null);
@@ -110,32 +110,32 @@ function validateNumberPlacement(index, numbers) {
     // getting the current number and its neighbors
     const currentNumber = numbers[index];
     const tileNeighbors = neighbors[index + 1];
-    
+
     // checking all neighbors
     for (const neighbor of tileNeighbors) {
         const neighborIndex = neighbor - 1;
         const neighborNumber = numbers[neighborIndex];
-        
+
         if (neighborNumber === null) continue; // empty field
-        
+
         // if rule1 is turned off 6&8 can't touch
-        if (!rule1 && ((currentNumber === 6 && neighborNumber === 8) || 
-                      (currentNumber === 8 && neighborNumber === 6))) {
+        if (!rule1 && ((currentNumber === 6 && neighborNumber === 8) ||
+            (currentNumber === 8 && neighborNumber === 6))) {
             return false;
         }
-        
+
         // if rule2 is turned off 2&12 can't touch
-        if (!rule2 && ((currentNumber === 2 && neighborNumber === 12) || 
-                      (currentNumber === 12 && neighborNumber === 2))) {
+        if (!rule2 && ((currentNumber === 2 && neighborNumber === 12) ||
+            (currentNumber === 12 && neighborNumber === 2))) {
             return false;
         }
-        
+
         // if rule3 is turned off the same number can't touch
         if (!rule3 && currentNumber === neighborNumber) {
             return false;
         }
     }
-    
+
     return true; // everything is good, all rules are met
 }
 
@@ -154,9 +154,17 @@ function shuffleArray(array) {
 // returns the dice odd of the given number
 function RollCalculator(num) {
     const rollValues = {
-        2: 1, 3: 2, 4: 3, 5: 4, 
-        6: 5, 7: 6, 8: 5, 
-        9: 4, 10: 3, 11: 2, 12: 1
+        2: 1,
+        3: 2,
+        4: 3,
+        5: 4,
+        6: 5,
+        7: 6,
+        8: 5,
+        9: 4,
+        10: 3,
+        11: 2,
+        12: 1
     };
     return rollValues[num];
 }
